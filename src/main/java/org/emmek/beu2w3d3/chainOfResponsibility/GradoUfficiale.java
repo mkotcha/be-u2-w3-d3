55df1dcf9b62a4dac55f27f412b3564f8105dd88
Matteo Falasca <matteo.falasca@emmek.org>
2023-11-15 16:17:57 +0100

    Es 3 chain

---
+++ src/main/java/org/emmek/beu2w3d3/chainOfResponsibility/GradoUfficiale.java
@@ -0,0 +1,21 @@
+package org.emmek.beu2w3d3.chainOfResponsibility;
+
+public class GradoUfficiale extends Ufficiale {
+
+    private String grado;
+
+    private double stipendio;
+
+    public GradoUfficiale(String grado, double stipendio) {
+        this.grado = grado;
+        this.stipendio = stipendio;
+    }
+
+    @Override
+    public boolean check(double amount) {
+        if (stipendio >= amount) {
+            System.out.println("Il " + grado + " percepisce almeno " + amount);
+        }
+        return checkNext(amount);
+    }
+}
---
+++ src/main/java/org/emmek/beu2w3d3/chainOfResponsibility/Ufficiale.java
@@ -0,0 +1,23 @@
+package org.emmek.beu2w3d3.chainOfResponsibility;
+
+public abstract class Ufficiale {
+    private Ufficiale next;
+
+    public static Ufficiale link(Ufficiale first, Ufficiale... ufficiali) {
+        Ufficiale head = first;
+        for (Ufficiale ufficiale : ufficiali) {
+            head.next = ufficiale;
+            head = ufficiale;
+        }
+        return first;
+    }
+
+    public abstract boolean check(double amount);
+
+    protected boolean checkNext(double amount) {
+        if (next == null) {
+            return true;
+        }
+        return next.check(amount);
+    }
+}
